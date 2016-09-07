1c1d7ea398c960791e4bf97d9612e0ce76e68daf
Connor Chevli <connorch@buffalo.edu>
2016-09-06 17:50:30 -0700

    Write puzzle schema.  Create collection/model for Puzzle with mongoose.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+
+/*************************
+        SERVER
+*************************/
+
 var app = express();
 
+// Allows us to access the request data on req.body.
 app.use(bodyParser.json());
+// Allows us to access the request url and parameters
+// on req.url and req.params.
 app.use(bodyParser.urlencoded({extended: true}));
 
 var port = 1234;
@@ -12,6 +20,10 @@ app.listen(port, function() {
   console.log('Now listening on Port: ' + port);
 });
 
+
+/*************************
+        DATABASE
+*************************/
 
 var databasePath = 'mongodb://localhost/treasure-hunt-database';
 mongoose.connect(databasePath);
@@ -23,5 +35,19 @@ db.on('error', function(err) {
 });
 
 db.once('open', function() {
-  console.log('Connection to database successful');
+  console.log('Connection to database successful.');
 });
+
+// Schemas:
+var puzzleSchema = new mongoose.Schema{
+  id: Number,
+  next: Number,
+  location: {
+    latitude: Number,
+    Longitude: Number
+  },
+  riddle: String,
+  answers: [String]
+}
+
+var Puzzle = mongoose.model('Puzzle', puzzleSchema);
