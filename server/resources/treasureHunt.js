da508d51f15631d394d98b6f3429fae02233c243
Bradford Melluish <bradmelluish@gmail.com>
2016-09-08 15:57:58 -0700

    Implements Schemas and Server File Structure
    Router and Controller coming soon.

--- server/resources/treasureHunt.js
+++ server/resources/treasureHunt.js
@@ -8,16 +8,8 @@ var puzzleSchema = new mongoose.Schema({
   teasureHuntTitle: String,
   // Unique IDs of the current, previous, and next puzzles
   // (like a linked list of puzzles).
-  head: {
-    type: Boolean,
-    default: false
-  },
-  tail: {
-    type: Boolean, 
-    default: false
-  },
-  next: String,
-  previous: String,
+  next: String, //if last, next == tail
+  previous: String, //if first, next == head
   // Location where user must be to receive/solve the riddle.
   location: {
     latitude: Number,
@@ -32,18 +24,13 @@ var puzzleSchema = new mongoose.Schema({
   },
   // The title of the puzzle
   // ( ex: 'The Goat of Hack Reactor' );
-  title: String,
+  riddleTitle: String,
   // The riddle that must be solved.
   // ( ex: 'What lies beneith the stone goat?' )
-  riddle: String,
+  riddleContent: String,
   // An array of acceptable answers to the riddle.
   // ( ex: ['wreath', 'wreaths', 'stone wreath', 'stone wreaths', 'garland', 'garlands', 'stone garland' ...] )
-  answers: [String],
-  // Date that the puzzle schema is created (not used for anything at the moment).
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  riddleAnswer: String 
 });
 
 module.exports = mongoose.model('Puzzle', puzzleSchema);
