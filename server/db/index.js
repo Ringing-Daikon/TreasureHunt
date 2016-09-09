97af5772c3bb1c6faa46aaa7f85c6ae9df0615a1
Bradford Melluish <bradmelluish@gmail.com>
2016-09-09 16:43:44 -0700

    Server/DB ES6 Refactor2

--- server/db/index.js
+++ server/db/index.js
@@ -15,13 +15,13 @@ db.once('open', () => {
   console.log(chalk.magenta.bold('Loading Default Data...'));
   var outputs = [];
   require('../data/default.json')
-    .forEach(function(puzzleObj, index, arr) {
-      new require('../resources/Puzzle.js')(puzzleObj).save(function (err, data) {
+    .forEach((puzzleObj, index, arr) => {
+      new require('../resources/Puzzle.js')(puzzleObj).save((err, data) => {
         err ? 
           outputs.push(chalk.yellow(err)) 
           : outputs.push(chalk.green(data));
         if(index === arr.length - 1) {
-          outputs.forEach(function(output) {
+          outputs.forEach(output => {
             console.log(output);
           });
           console.log(chalk.cyan.underline.bold('Load Complete.'), chalk.grey.inverse.bold('\nDuplicate errors indicate data has already been populated, otherwise loaded documents will be displayed.'));
