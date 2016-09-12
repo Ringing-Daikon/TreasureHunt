0e6801bcbd2ef16b345df29f0599857531f65783
Bradford Melluish <bradmelluish@gmail.com>
2016-09-11 17:03:09 -0700

    Formats Util.js for Readability

--- app/util/util.js
+++ app/util/util.js
@@ -1,50 +1,76 @@
 const server = 'http://localhost:1337/api/puzzles';
 //can retrieve a puzzle from a treasure hunt, all puzzles in a treasure hunt, or all puzzles
+
 export var retrievePuzzles = (treasureHuntTitle = null, riddleTitle = null, cb = null) => {
-  var serverPath = server; //create a local instance of server
+  var serverPath = server; //create local server variable that can be modified
+
+  //check for cb, lets user just put cb instead of nulls infront for riddleTitle and treasureHuntTitle
   if(typeof treasureHuntTitle === 'function')
-    cb = treasureHuntTitle, treasureHuntTitle = null;
+    cb = treasureHuntTitle, 
+    treasureHuntTitle = null;
   if(typeof riddleTitle === 'function')
-    cb = riddleTitle, riddleTitle = null;
+    cb = riddleTitle, 
+    riddleTitle = null;
   if(treasureHuntTitle !== null) {
     serverPath += `/${treasureHuntTitle.replace(/\s/g, '%20')}`;
     if(riddleTitle !== null)
-      serverPath += `/${riddleTitle.replace(/\s/g, '%20')}`;} 
+      serverPath += `/${riddleTitle.replace(/\s/g, '%20')}`;
+  } 
+
+  //HTTP GET request
   fetch(serverPath, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'}})
+      'Content-Type': 'application/json'
+    }
+  })
   .then(response => response.json())
   .then(responseJson => cb && cb(responseJson))
-  .catch(error => console.error(error));};
+  .catch(error => console.error(error));
+};
+
 //should take an array of puzzle objects, will then stringify before sending. 
 export var createPuzzles = (puzzleArray = [], cb = null) => {
+  //HTTP POST request
   fetch(server, {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'}, 
-    body: JSON.stringify(puzzleArray)})
+    body: JSON.stringify(puzzleArray)
+  })
   .then(response => response.json())
   .then(responseJson => cb && cb(responseJson))
-  .catch(error => console.error(error));};
+  .catch(error => console.error(error));
+};
+
 //can delete a puzzle from a treasure hunt, all puzzles in a treasure hunt, or all puzzles
 export var deletePuzzles = (treasureHuntTitle = null, riddleTitle = null, cb = null) => {
-  var serverPath = server; //create a local instance of server
+  var serverPath = server; //create local server variable that can be modified
+
+  //check for cb, lets user just put cb instead of nulls infront for riddleTitle and treasureHuntTitle
   if(typeof treasureHuntTitle === 'function')
-    cb = treasureHuntTitle, treasureHuntTitle = null;
+    cb = treasureHuntTitle, 
+    treasureHuntTitle = null;
   if(typeof riddleTitle === 'function')
-    cb = riddleTitle, riddleTitle = null;
+    cb = riddleTitle, 
+    riddleTitle = null;
   if(treasureHuntTitle !== null) {
     serverPath += `/${treasureHuntTitle.replace(/\s/g, '%20')}`;
     if(riddleTitle !== null)
-      serverPath += `/${riddleTitle.replace(/\s/g, '%20')}`;} 
+      serverPath += `/${riddleTitle.replace(/\s/g, '%20')}`;
+  } 
+
+  //HTTP DELETE request
   fetch(serverPath, {
     method: 'DELETE',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'}})
+      'Content-Type': 'application/json'
+    }
+  })
   .then(response => response.json())
   .then(responseJson => cb && cb(responseJson))
-  .catch(error => console.error(error));};
+  .catch(error => console.error(error));
+};
--- server/resources/controller.js
+++ server/resources/controller.js
@@ -10,7 +10,7 @@ module.exports = {
     Puzzle.find({treasureHuntTitle: req.params.treasureHuntTitle, riddleTitle: req.params.riddleTitle}, (err, data) => err ? 
       res.status(404).send(err) : res.status(200).send(JSON.stringify(data)));}, 
   createPuzzles (req, res) { //takes an array of puzzles and return the array of puzzles created
-    var dataArr = [];
+    let dataArr = [];
     req.body.forEach((puzzleObj, index) => new Puzzle(puzzleObj).save((err, data) => {
         err ? res.status(500).send(err) : dataArr.push(data);
         index === req.body.length - 1 && res.status(201).send(JSON.stringify(dataArr));}));}, 
